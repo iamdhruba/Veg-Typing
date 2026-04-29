0490521740149903e7cc15619bcb4fde00c91f0c
medhruba <dhrubarajchaudhary498@gmail.com>
2026-04-29 23:59:12 +0545

    fix: remove frontend static serving, frontend is deployed on Vercel

--- backend/index.js
+++ backend/index.js
@@ -61,19 +61,7 @@ app.use('/api/leaderboard', require('./routes/leaderboard.js'));
 // Socket.io
 require('./socketHandlers')(io);
 
-// Serve frontend in production
-if (process.env.NODE_ENV === 'production') {
-  const path = require('path');
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
-
-  app.get('/{*path}', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'dist', 'index.html')
-    )
-  );
-} else {
-  app.get('/', (req, res) => res.send('API is running...'));
-}
+app.get('/', (req, res) => res.send('API is running...'));
 
 // Global error handler
 app.use((err, req, res, next) => {
